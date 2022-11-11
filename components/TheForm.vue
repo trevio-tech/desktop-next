dcfcdfd227274767d74d7eb6df060ed2ec9da1a7
Denis <zabavnikov.com@gmail.com>
2022-11-11 14:02:31 +0700

    update

---
+++ components/TheForm.vue
@@ -0,0 +1,27 @@
+<template>
+  <form @submit.prevent="$emit('submit')" class="min-h-full flex flex-col justify-between bg-white p-4 rounded-md shadow ring-1 ring-slate-200">
+    <div class="space-y-4">
+      <slot></slot>
+    </div>
+    <footer class="mt-8">
+      <hr class="mb-4 -mx-4 border-gray-200">
+
+      <div class="flex space-x-2">
+        <Button type="submit">{{ isEdit ? 'Сохранить' : 'Создать' }}</Button>
+        <Button @click="$emit('draft', true)" type="submit" variant="secondary">Сохранить в черновик</Button>
+      </div>
+    </footer>
+  </form>
+</template>
+
+<script setup>
+import { VButton as Button } from '@trevio/ui';
+
+defineEmits(['submit', 'draft'])
+
+defineProps({
+  isEdit: {
+    type: Boolean,
+  }
+})
+</script>
