d85b47ac4de7fbe57b3bea2fdf172cb514597c9c
Denis <zabavnikov.com@gmail.com>
2023-01-19 17:21:58 +0700

    Доработки редактора шотов.

--- components/modules/shots/components/ShotFormDialog.vue
+++ components/modules/shots/components/ShotFormDialog.vue
@@ -4,8 +4,12 @@
       <div class="bg-white flex-shrink-0 w-[480px] h-full rounded-tl-lg" id="shot-container">
         <canvas id="shot"></canvas>
       </div>
-      <div class="w-[320px] h-full bg-gray-100 rounded-tr-lg">
-        <div @click="addText">addText</div>
+      <div class="w-[320px] h-full bg-gray-100 rounded-tr-lg p-2">
+        <Button @click="addText" class="w-full">Добавит текст</Button>
+        <Button @click="changeFillColor('red')" class="w-full">Красный</Button>
+        <Button @click="changeFillColor('green')" class="w-full">Зеленый</Button>
+        <Button @click="changeFillColor('yellow')" class="w-full">Желтый</Button>
+        <Button @click="changeFillColor('orange')" class="w-full">Оранжевый</Button>
       </div>
     </div>
     <footer class="p-4 bg-gray-100 w-full rounded-b-lg"></footer>
@@ -13,8 +17,6 @@
 </template>
 
 <script setup>
-import useCanvas from '~/components/modules/shots/composables/useCanvas'
-import { X } from 'lucide-vue-next'
-const { addText } = useCanvas()
-
+import { useShotEditor } from '#imports'
+const { addText, changeFillColor } = useShotEditor()
 </script>
