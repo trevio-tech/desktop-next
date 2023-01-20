f43a270a7e7511df24eb7dae8f1ac7fb6ac6174d
Denis <zabavnikov.com@gmail.com>
2023-01-20 15:52:44 +0700

    Доработка создания шота.

---
+++ components/modules/shots/components/ShotEditorTrash.vue
@@ -0,0 +1,37 @@
+<template>
+  <div @mouseenter="onMouseEnter" @mouseleave="onMouseLeave" class="bg-black/40 rounded-full w-[80px] h-[80px] flex items-center justify-center ring-4 ring-white shadow">
+    <div id="trash-json"></div>
+  </div>
+</template>
+
+<style>
+#trash-json path {
+  stroke: white;
+}
+</style>
+
+<script setup>
+import { onMounted } from 'vue'
+import bodymovin from 'lottie-web'
+import TrashIcon from './trash.json'
+
+let icon = null
+
+onMounted(() => {
+  icon = bodymovin.loadAnimation({
+    container: document.getElementById('trash-json'),
+    animationData: TrashIcon,
+    renderer: 'svg',
+    loop: true,
+    autoplay: false,
+    name: 'trashIcon',
+  })
+})
+
+const onMouseEnter = () => {
+  icon.play()
+}
+const onMouseLeave = () => {
+  icon.stop()
+}
+</script>
--- components/modules/shots/components/ShotFormDialog.vue
+++ components/modules/shots/components/ShotFormDialog.vue
@@ -1,22 +1,39 @@
 <template>
   <div class="place-self-center flex flex-col items-center justify-center h-full p-4">
     <div class="h-full flex">
-      <div class="bg-white flex-shrink-0 w-[480px] h-full rounded-tl-lg" id="shot-container">
+      <div class="bg-white flex-shrink-0 w-[480px] h-full rounded-tl-lg relative" id="shot-container">
         <canvas id="shot"></canvas>
+
+        <ShotEditorTrash
+          v-if="isTrash"
+          @mouseup="addToTrash" id="trash" class="absolute" style="bottom: 20px; left: calc(50% - 40px)" />
       </div>
-      <div class="w-[320px] h-full bg-gray-100 rounded-tr-lg p-2">
+
+      <div class="w-[320px] h-full bg-gray-100 rounded-tr-lg p-4">
         <Button @click="addText" class="w-full">Добавит текст</Button>
-        <Button @click="changeFillColor('red')" class="w-full">Красный</Button>
-        <Button @click="changeFillColor('green')" class="w-full">Зеленый</Button>
-        <Button @click="changeFillColor('yellow')" class="w-full">Желтый</Button>
-        <Button @click="changeFillColor('orange')" class="w-full">Оранжевый</Button>
+
+        <h3 class="mt-4 mb-2 font-bold text-wh">Цвет</h3>
+        <div class="grid grid-cols-4 gap-2">
+          <div
+            v-for="color in colors"
+            :key="color"
+            :style="{backgroundColor: color}"
+            class="w-16 h-16 rounded-full cursor-pointer ring-2 ring-white"
+            @click="changeFillColor(color)"
+          >
+          </div>
+        </div>
       </div>
     </div>
-    <footer class="p-4 bg-gray-100 w-full rounded-b-lg"></footer>
+    <footer class="p-4 bg-gray-50 w-full rounded-b-lg"></footer>
   </div>
 </template>
 
 <script setup>
 import { useShotEditor } from '#imports'
-const { addText, changeFillColor } = useShotEditor()
+import ShotEditorTrash from '~/components/modules/shots/components/ShotEditorTrash.vue'
+
+const { addText, changeFillColor, addToTrash, isTrash } = useShotEditor()
+
+const colors = ['white', 'black', 'red', 'green', 'yellow', 'orange']
 </script>
