c770876df656405522ce742eb2bcbbead244b71f
Denis <zabavnikov.com@gmail.com>
2023-03-16 09:05:05 +0700

    пока скрыл чаты.

--- components/modules/chats/components/ChatDialog.vue
+++ components/modules/chats/components/ChatDialog.vue
@@ -1,13 +1,13 @@
 <template>
   <div tabindex="-1" aria-hidden="true" class="absolute left-0 top-0 w-full h-full">
     <div class="flex items-center justify-start flex-col p-10 h-full">
-      <ChatD class="max-w-[804px]" :chat-id="chatId" :title="title" />
+<!--      <ChatD1 class="max-w-[804px]" :chat-id="chatId" :title="title" />-->
     </div>
   </div>
 </template>
 
 <script setup>
-import ChatD from './Chat/ChatD.vue'
+// import ChatD1 from './Chat/ChatD1.vue'
 
 const props = defineProps({
   chatId: {
