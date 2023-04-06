6a19c3ec70c9943847f0cd44426d71bc564f355b
Denis <zabavnikov.com@gmail.com>
2023-04-06 13:54:56 +0700

    Добавил кнопку показать еще в списке заметок.

--- components/modules/notes/pages/index.vue
+++ components/modules/notes/pages/index.vue
@@ -4,66 +4,53 @@
     <div class="space-y-4">
       <ContentCard v-for="note in notes" :entry="note" :key="note.id"/>
     </div>
+    <Button :loading="isFetching" @click="fetchNotes" variant="secondary" class="w-full my-4">Показать еще</Button>
   </NuxtLayout>
 </template>
 
 <script setup>
 import ContentCard from '~/components/ContentCard'
-
 import { NOTE_CARD } from '../graphql'
-import { useRoute, useHead } from '#imports'
-import { ref, watch, computed } from 'vue'
-import { usePageQuery } from '@trevio/ui'
-
-const route = useRoute()
-
-const notes = ref([])
-const tag = ref(null)
-
-const title = computed(() => {
-  if (tag.value) {
-    return `Заметки по тегу ${tag.value.name}`
-  }
-
-  return 'Заметки'
-})
+import { ref, shallowRef } from 'vue'
+import { useHead } from '#imports'
+import { usePageQuery, Button } from '@trevio/ui'
 
 useHead({
-  title
+  title: 'Заметки'
 })
 
-const getNotes = async (tagId) => {
-  tagId = parseInt(tagId)
+const notes = ref([])
+const page = shallowRef(0)
+const isFetching = shallowRef(false)
 
-  const tagQuery = tagId > 0
-    ? `tag(id: $tag_tag_id) {
-      id
-      name
-    }` : ''
+const fetchNotes = async () => {
+  if (isFetching.value) return
 
-  const { data } = await usePageQuery({
-    query: `
-      query getNotes {
-        notes {
-          ${NOTE_CARD}
+  isFetching.value = true
+
+  page.value++
+
+  try {
+    const { data } = await usePageQuery({
+      query: `
+        query getNotes($page: Int) {
+          notes(page: $page) {
+            ${NOTE_CARD}
+          }
         }
-        ${tagQuery}
-      }
-    `,
-  })
+      `,
+      variables: {
+        page: page.value
+      },
+    })
 
-  notes.value = data.notes
-
-  if (data.tag) {
-    tag.value = data.tag
-  } else {
-    tag.value = null
+    notes.value = [...notes.value, ...data.notes]
+  } catch (error) {
+    console.log(error)
+  } finally {
+    isFetching.value = false
   }
 }
 
-await getNotes(route.query.tag_id)
-
-watch(() => route.query.tag_id, async (newTagId) => {
-  await getNotes(newTagId)
-})
+await fetchNotes()
 </script>
