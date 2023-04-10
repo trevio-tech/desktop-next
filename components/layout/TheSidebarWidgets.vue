4e040d7df80dc3bc0c1f00389650f1066cc351d9
Denis <zabavnikov.com@gmail.com>
2023-04-10 13:57:29 +0700

    wip

--- components/layout/TheSidebarWidgets.vue
+++ components/layout/TheSidebarWidgets.vue
@@ -1,7 +1,15 @@
 <template>
   <section>
     <h3 class="font-medium mb-2">Случайные заметки</h3>
-    <ul class="space-y-4">
+    <div v-if="notes.length === 0">
+      <Skeleton class="w-1/2 h-2" />
+      <Skeleton class="w-full h-2 mt-1" />
+
+      <Skeleton class="w-full h-3 mt-3" />
+      <Skeleton class="w-1/2 h-3 mt-1" />
+      <Skeleton class="w-full h-3 mt-1" />
+    </div>
+    <ul v-else class="space-y-4">
       <li v-for="note in notes" :key="note.id" class="text-sm">
         <NuxtLink :to="{name: 'notes.show', params: {noteId: note.id}}" class="font-medium hover:underline">{{ note.title }}</NuxtLink>
         <div v-if="note.text" class="mt-1">
@@ -13,14 +21,15 @@
 </template>
 
 <script setup>
-import { useQuery } from '@trevio/ui'
-import { shallowRef } from 'vue'
+import { Skeleton, useQuery } from '@trevio/ui'
+import { shallowRef, onBeforeMount } from 'vue'
 
 const notes = shallowRef([])
 
-try {
-  const { data } = await useQuery({
-    query: `
+onBeforeMount(async () => {
+  try {
+    const { data } = await useQuery({
+      query: `
       query sidebarWidgets {
         notes(random: true, limit: 5) {
           id
@@ -29,10 +38,11 @@ try {
         }
       }
     `
-  })
+    })
 
-  notes.value = data.notes
-} catch (error) {
-  console.log(error)
-}
+    notes.value = data.notes
+  } catch (error) {
+    console.log(error)
+  }
+})
 </script>
